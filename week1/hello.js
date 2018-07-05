12c503be06c60e7ff8fbf0f9bd5e23606e63b8df
Shadi Amr <35688557+shadoo77@users.noreply.github.com>
2018-07-05 21:03:11 +0200

    codes to hello file

---
+++ week1/hello.js
@@ -0,0 +1,6 @@
+console.log('Hello, world'); // English
+console.log('Hallo, wereld'); // Nederland
+console.log('مرحبا, العالم'); // Arabic
+console.log('Halo, dunia!'); // Indonesian
+console.log('Ciao, mondo!'); // Italian
+console.log('Hola, mundo!'); // Spanish
