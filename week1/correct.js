a6a317adabca492a42f1e3c38a503fa4f03af795
Shadi Amr <35688557+shadoo77@users.noreply.github.com>
2018-07-05 21:04:52 +0200

    correct.js file adding

---
+++ week1/correct.js
@@ -0,0 +1,16 @@
+console.log("I'm awesome");
+var x = 5;
+// here you initialize your variable
+console.log('the value of my variable x will be 5');
+console.log(x);
+//  here assign 10 to my variable 
+x = 10;
+console.log('the value of my variable x will be 10');
+console.log(x);
+// here is a new variable assign a text
+console.log('the value of my variable y will be a text : "My test new variable"');
+var y = "My test new variable";
+console.log(y);
+console.log('the value of my variable y will be : "A new text here"');
+y = "A new text here";
+console.log(y);
