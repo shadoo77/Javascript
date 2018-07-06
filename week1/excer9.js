30c3142d089360abb31210096c9794f4a20974f2
Shadi Amr <35688557+shadoo77@users.noreply.github.com>
2018-07-06 19:48:25 +0200

    add excer9.js file

---
+++ week1/excer9.js
@@ -0,0 +1,5 @@
+let x = 7;
+x = x % 3;
+console.log("The % operator is a remainder operator and is used to get the remainder after integer division");
+console.log("So in our example we have to divide variable x that equal 7 to 3 : 7/3 = 2\n we have extra 1 ! so the x = 1");
+console.log("The x value became : " + x);
