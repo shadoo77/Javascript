aded31e370b3b99a7a38de76f0644c5474925499
Shadi Amr <35688557+shadoo77@users.noreply.github.com>
2018-07-06 23:31:41 +0200

    add excer10.js file

---
+++ week1/excer10.js
@@ -0,0 +1,9 @@
+console.log("Sure i can add multiple types of data to an array, this is an example :");
+let myArray = ["name", "shadi", 34, true, {age: 34}];
+for (var i = 0; i < myArray.length; i++) {
+console.log(myArray[i]);
+}
+/* ***************** */
+var a = 6 / 0;
+var b = 10 / 2;
+(a === b) ? console.log("It's same result") : console.log("not same");
